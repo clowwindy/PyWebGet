1b347e71e112262d56a676a73d168bad3b050170
U-clowwindy-win7\clowwindy <clowwindy@clowwindy-win7.(none)>
2011-11-20 16:03:13 +0800

    basic authorization

--- webui/app.py
+++ webui/app.py
@@ -6,15 +6,22 @@ from core import task, version
 import time
 import utils
 import json
+import re
+import base64
 
 def common_setup():
     web.header('Server', "%s/%s" % (version.APP_NAME, version.VERSION))
+    if web.ctx.env.get('HTTP_AUTHORIZATION') is not None:
+        return 'This is the index page'
+    else:
+        raise web.seeother('/login')
 
 class index:
     def GET(self):
         common_setup()
         return web.seeother("/static/")
-    
+
+
 class add_task:
     def POST(self):
         global controller
@@ -28,6 +35,7 @@ class add_task:
         except Exception, e:
             return json.dumps(unicode(e))
 
+
 class pause_tasks:
     def POST(self):
         global controller
@@ -39,7 +47,8 @@ class pause_tasks:
             return '"OK"'
         except Exception, e:
             return json.dumps(unicode(e))
-        
+
+
 class resume_tasks:
     def POST(self):
         global controller
@@ -51,7 +60,8 @@ class resume_tasks:
             return '"OK"'
         except Exception, e:
             return json.dumps(unicode(e))
-        
+
+
 class remove_tasks:
     def POST(self):
         global controller
@@ -64,21 +74,45 @@ class remove_tasks:
         except Exception, e:
             return json.dumps(unicode(e))
 
+
 class task_list:
     def GET(self):
         global controller
         tasks = controller.task_list()
         for a_task in tasks:
-#            a_task.status = task.str_by_status(a_task.status)
-#            a_task.checkbox = "<input type='checkbox' id='task_%d' />" % a_task.id
+        #            a_task.status = task.str_by_status(a_task.status)
+        #            a_task.checkbox = "<input type='checkbox' id='task_%d' />" % a_task.id
             a_task.percent = "100%"
             a_task.percent = "100%"
-#            a_task.date_completed = utils.timestamp_repr(a_task.date_completed)
-#            a_task.date_created = utils.timestamp_repr(a_task.date_created)
+        #            a_task.date_completed = utils.timestamp_repr(a_task.date_completed)
+        #            a_task.date_created = utils.timestamp_repr(a_task.date_created)
         import json
+
         web.header('Content-Type', 'application/json')
         common_setup()
-        return json.dumps({"tasks":tasks})
+        return json.dumps({"tasks": tasks})
+
+allowed = (
+    ('pywebget','password'),
+)
+
+class login:
+    def GET(self):
+        auth = web.ctx.env.get('HTTP_AUTHORIZATION')
+        authreq = False
+        if auth is None:
+            authreq = True
+        else:
+            auth = re.sub('^Basic ', '', auth)
+            username, password = base64.decodestring(auth).split(':')
+            if (username, password) in allowed:
+                raise web.seeother('/')
+            else:
+                authreq = True
+        if authreq:
+            web.header('WWW-Authenticate', 'Basic realm="PyWebGet"')
+            web.ctx.status = '401 Unauthorized'
+            return "401 Unauthorized"
 
 controller = None
 
@@ -86,15 +120,17 @@ def set_controller(c):
     global controller
     controller = c
 
+
 def run():
     urls = (
-      '/', 'index',
-      '/task_list', 'task_list',
-      '/add_task', 'add_task',
-      '/pause_tasks', 'pause_tasks',
-      '/resume_tasks', 'resume_tasks',
-      '/remove_tasks', 'remove_tasks',
-    )
+        '/', 'index',
+        '/task_list', 'task_list',
+        '/add_task', 'add_task',
+        '/pause_tasks', 'pause_tasks',
+        '/resume_tasks', 'resume_tasks',
+        '/remove_tasks', 'remove_tasks',
+        '/login', 'login',
+        )
 
-    my_app = web.application(urls, globals(),autoreload=False)
+    my_app = web.application(urls, globals(), autoreload=False)
     my_app.run()
