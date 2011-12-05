c86e9cf9bb6cae5484a1e7a6c3910b2b763d1fb4
clowwindy <clowwindy@clowwindy-ThinkPad-E420.(none)>
2011-12-05 14:34:48 +0800

    remove unnecessary test

--- test/test_wget_parser.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-__author__ = 'clowwindy'
-
-
-from core import wget_parser
-
-print wget_parser.parse_args(u'wget -o CrystalDiskInfo4你好_1_3-en.exe --header="Cookie: lx_login_u=clowwindy; lx_login_auto=1; gdriveid=015576B414E964A2C77DF87A1470A556" --referer="http://lixian.vip.xulei.com/" "http://gdl.lixian.vip.xunlei.com/download?fid=D7iJ8CYbsYnZJp3YyMpg7Auk0zWQIBoAAAAAAKR3v9ij9w2CpMgGIOZ3IDwSEK5N&mid=666&threshold=150&tid=89F300566E1799F7E1D2AC643A4385DE&srcid=4&verno=1&g=A477BFD8A3F70D82A4C80620E677203C1210AE4D&scn=c8&i=A477BFD8A3F70D82A4C80620E677203C1210AE4D&t=0&ui=28118228&ti=41286564353&s=1712272&m=0&n=0122439D2C74616C4408428F166E666F343E00BB6C2D656E2E0449815F00000000"')
